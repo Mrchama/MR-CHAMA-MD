b674732c6db05c91c1ecefb376a59e7cd33d909a
MR CHAMALKA <146830698+Mrchama@users.noreply.github.com>
2024-06-09 13:21:02 +0530

    Update _onTEXT.js

--- Plugins/_onTEXT.js
+++ Plugins/_onTEXT.js
@@ -4,15 +4,15 @@
 * @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
 * @link <https://github.com/Mrchama/MR-CHAMA-MD>
 * @version 1.0.0
-* @file  _anti_functions.js - MrChamaMd on text commands
+* @file  _onTEXT.js - MrChamaMd on text commands
 
 © 2024 Mr Chamalka , ANTECH. All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
 
-const { MR-CHAMA-MD, chamamdChat, Language } = require('mr_cham_md_core/dist/scripts')
-const { semiAIchat } = amdiChat
+const { MRCHAMAMD, chamamdChat, Language } = require('mr_cham_md_core/dist/scripts')
+const { semiAIchat } = chamamdChat
 const Lang = Language.getString('botCTRL');
 
 CHAMAMD({ onText: "MR CHAMA MD", desc: Lang.AI_USAGE, example: Lang.AI_EXAMPLE, type: "primary", react: "👾" }, (async (chamamdWA) => {
