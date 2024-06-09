3a00d541f2ce7765992f93b35bcdaf249402905b
MR CHAMALKA <146830698+Mrchama@users.noreply.github.com>
2024-06-09 12:39:44 +0530

    Update _onTEXT.js

--- Plugins/_onTEXT.js
+++ Plugins/_onTEXT.js
@@ -1,29 +1,30 @@
 /**
-* @project_name Queen Amdi [WA Multi-device]
-* @author BlackAmda <https://github.com/BlackAmda>
+* @project_name Mr Chama Md [WA Multi-device]
+* @author MrChamalka <https://github.com/Mrchama>
 * @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
-* @link <https://github.com/BlackAmda/QueenAmdi>
-* @version 4.0.8
-* @file  _onTEXT.js - QueenAmdi on text commands
+* @link <https://github.com/Mrchama/MR-CHAMA-MD>
+* @version 1.0.0
+* @file  _anti_functions.js - MrChamaMd on text commands
 
-© 2022 Black Amda, ANTECH. All rights reserved.
+© 2024 Mr Chamalka , ANTECH. All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
-const { AMDI, amdiChat, Language } = require('queen_amdi_core/dist/scripts')
+
+const { MR-CHAMA-MD, chamamdChat, Language } = require('mr_cham_md_core/dist/scripts')
 const { semiAIchat } = amdiChat
 const Lang = Language.getString('botCTRL');
 
-AMDI({ onText: "Amdi", desc: Lang.AI_USAGE, example: Lang.AI_EXAMPLE, type: "primary", react: "🤖" }, (async (amdiWA) => {
-    let { input, reply } = amdiWA.msgLayout
+CHAMAMD({ onText: "MR CHAMA MD", desc: Lang.AI_USAGE, example: Lang.AI_EXAMPLE, type: "primary", react: "👾" }, (async (chamamdWA) => {
+    let { input, reply } = chamamdWA.msgLayout
 
     if (!input) return await reply(`${Lang.AI_USAGE}\n\nExample: ${Lang.AI_EXAMPLE}`);
-    await semiAIchat( amdiWA );
+    await semiAIchat( chamamdWA );
 }));
 
-AMDI({ onText: "ඇම්ඩි", desc: "AI Chat bot", type: "primary", react: "🤖", cmdHideInMenu: true }, (async (amdiWA) => {
-    let { input, reply } = amdiWA.msgLayout
+CHAMAMD({ onText: "චමා", desc: "AI Chat bot", type: "primary", react: "👾", cmdHideInMenu: true }, (async (chamamdWA) => {
+    let { input, reply } = chamamdWA.msgLayout
 
     if (!input) return await reply(`${Lang.AI_USAGE}\n\nExample: ${Lang.AI_EXAMPLE}`);
-    await semiAIchat( amdiWA );
+    await semiAIchat( chamamdWA );
 }));
