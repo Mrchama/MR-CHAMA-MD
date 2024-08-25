00ea9d60497955a2739477fe0ec9c48c41b566d7
MR CHAMALKA <146830698+Mrchama@users.noreply.github.com>
2024-08-25 14:28:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_EMG: process.env.ALIVE_IMG || "පොටෝ එකක Url එකක් දාන්න",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am Mrchamalka i am alive now!",
 AUTO_READ_STATUS:process.env.AUTO_READ_STATUS || "true",
+MOOD:process.env.MOOD || "public",
 }:
